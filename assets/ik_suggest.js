2588aa84f5d40b0d15a8afc6547f029acffd891a
Tristan Price <prob.fine@gmail.com>
2018-10-05 15:24:15 -0700

    Final(Part 2)

--- assets/ik_suggest.js
+++ assets/ik_suggest.js
@@ -1,14 +1,15 @@
-;(function ( $, window, document, undefined ) {
- 
-var pluginName = "ik_suggest",
-	defaults = {
-		'instructions': "As you start typing the application might suggest similar search terms. Use up and down arrow keys to select a suggested search string.",
-		'minLength': 2,
-		'maxResults': 10,
-		'source': []
-		
-	};
-	
+;
+(function ($, window, document, undefined) {
+
+	var pluginName = "ik_suggest",
+		defaults = {
+			'instructions': "As you start typing the application might suggest similar search terms. Use up and down arrow keys to select a suggested search string.",
+			'minLength': 2,
+			'maxResults': 10,
+			'source': []
+
+		};
+
 	/**
 	 * @constructs Plugin
 	 * @param {Object} element - Current DOM element from selected collection.
@@ -17,72 +18,71 @@ var pluginName = "ik_suggest",
 	 * @param {number} options.minLength - Mininmum string length before sugestions start showing.
 	 * @param {number} options.maxResults - Maximum number of shown suggestions.
 	 */
-	function Plugin( element, options ) { 
-		
+	function Plugin(element, options) {
+
 		this.element = $(element);
-		this.options = $.extend( {}, defaults, options);
+		this.options = $.extend({}, defaults, options);
 		this._defaults = defaults;
 		this._name = pluginName;
-		
+
 		this.init();
 	}
-	
+
 	/** Initializes plugin. */
 	Plugin.prototype.init = function () {
-		
+
 
 		var $elem, plugin;
-		
+
 		plugin = this;
-		
+
 		$elem = this.element
 			.attr({
 				'autocomplete': 'off'
 			})
-			.wrap('<span class="ik_suggest"></span>') 
-			.on('keydown', {'plugin': plugin}, plugin.onKeyDown) // add keydown event
-			.on('keyup', {'plugin': plugin}, plugin.onKeyUp) // add keyup event
-			.on('focusout', {'plugin': plugin}, plugin.onFocusOut);  // add focusout event
-		
+			.wrap('<span class="ik_suggest"></span>')
+			.on('keydown', {
+				'plugin': plugin
+			}, plugin.onKeyDown) // add keydown event
+			.on('keyup', {
+				'plugin': plugin
+			}, plugin.onKeyUp) // add keyup event
+			.on('focusout', {
+				'plugin': plugin
+			}, plugin.onFocusOut); // add focusout event
+
 		this.list = $('<ul/>')
 			.addClass('suggestions')
 			.attr({
 				'role': 'alert',
 				'aria-label': 'Suggested countries'
 			});
-		
+
 		this.notify = $('<div/>')
 			.addClass('ik_readersonly')
 			.attr({
 				'role': 'region',
-				'aria-live': 'polite',
+				'aria-live':'polite',
+				'id': 'instructions'
 			})
-			.append.options.instructions;
-	
+			.text(plugin.options.instructions);
+
 
 		$elem.after(this.notify, this.list);
 
-		
-				
+
+
 	};
 
+	Plugin.prototype.onFocus = function (event) {
+		
+		var plugin;
 
-		/** 
-		 * Handles focus event on text field.
-		 * 
-		 * @param {object} event - Keyboard event.
-		 * @param {object} event.data - Event data.
-		 * @param {object} event.data.plugin - Reference to plugin.
-		 */
-		Plugin.prototype.onFocus = function (event) {
+		plugin = event.data.plugin;
 
-			var plugin;
+		plugin.notify.text(plugin.options.instructions);
 
-			plugin = event.data.plugin;
-
-			plugin.notify.text(plugin.options.instructions);
-
-		};
+	};
 
 	/** 
 	 * Handles keydown event on text field.
@@ -92,32 +92,32 @@ var pluginName = "ik_suggest",
 	 * @param {object} event.data.plugin - Reference to plugin.
 	 */
 	Plugin.prototype.onKeyDown = function (event) {
-		
+
 		var plugin, selected;
-		
+
 		plugin = event.data.plugin;
-		
+
 		switch (event.keyCode) {
-			
+
 			case ik_utils.keys.tab:
 			case ik_utils.keys.esc:
-								
+
 				plugin.list.empty().hide(); // empty list and hide suggestion box
-					
+
 				break;
-			
+
 			case ik_utils.keys.enter:
-				
+
 				selected = plugin.list.find('.selected');
-				plugin.element.val( selected.text() ); // set text field value to the selected option
+				plugin.element.val(selected.text()); // set text field value to the selected option
 				plugin.list.empty().hide(); // empty list and hide suggestion box
-				
+
 				break;
-				
+
 		}
-		
+
 	};
-	
+
 	/** 
 	 * Handles keyup event on text field.
 	 * 
@@ -126,9 +126,9 @@ var pluginName = "ik_suggest",
 	 * @param {object} event.data.plugin - Reference to plugin.
 	 */
 	Plugin.prototype.onKeyUp = function (event) {
-		
+
 		var plugin, $me, suggestions, selected, msg;
-		
+
 		plugin = event.data.plugin;
 		$me = $(event.currentTarget);
 
@@ -154,32 +154,34 @@ var pluginName = "ik_suggest",
 				if (selected.length) {
 					msg = selected.removeClass('selected').prev().addClass('selected').text();
 				}
-				plugin.notify.text(msg);  // add suggestion text to live region to be read by screen reader
+				plugin.notify.text(msg); // add suggestion text to live region to be read by screen reader
 
 				break;
 
 			default: // get suggestions based on user input
-		
-			suggestions = plugin.getSuggestions(plugin.options.source, $me.val());
-				
+
+				suggestions = plugin.getSuggestions(plugin.options.source, $me.val());
+
 				if (suggestions.length) {
 					plugin.list.show();
 				} else {
 					plugin.list.hide();
 				}
-				
+
 				plugin.list.empty();
-				
-				for(var i = 0, l = suggestions.length; i < l; i++) {
+
+				for (var i = 0, l = suggestions.length; i < l; i++) {
 					$('<li/>').html(suggestions[i])
-					.on('click', {'plugin': plugin}, plugin.onOptionClick) // add click event handler
-					.appendTo(plugin.list);
+						.on('click', {
+							'plugin': plugin
+						}, plugin.onOptionClick) // add click event handler
+						.appendTo(plugin.list);
 				}
 
 				break;
-			}
+		}
 	};
-	
+
 	/** 
 	 * Handles fosucout event on text field.
 	 * 
@@ -188,13 +190,15 @@ var pluginName = "ik_suggest",
 	 * @param {object} event.data.plugin - Reference to plugin.
 	 */
 	Plugin.prototype.onFocusOut = function (event) {
-		
+
 		var plugin = event.data.plugin;
-		
-		setTimeout(function() { plugin.list.empty().hide(); }, 200);
-		
+
+		setTimeout(function () {
+			plugin.list.empty().hide();
+		}, 200);
+
 	};
-	
+
 	/** 
 	 * Handles click event on suggestion box list item.
 	 * 
@@ -203,19 +207,19 @@ var pluginName = "ik_suggest",
 	 * @param {object} event.data.plugin - Reference to plugin.
 	 */
 	Plugin.prototype.onOptionClick = function (event) {
-		
+
 		var plugin, $option;
-		
+
 		event.preventDefault();
 		event.stopPropagation();
-		
+
 		plugin = event.data.plugin;
 		$option = $(event.currentTarget);
-		plugin.element.val( $option.text() );
+		plugin.element.val($option.text());
 		plugin.list.empty().hide();
-		
+
 	};
-	
+
 	/** 
 	 * Gets a list of suggestions.
 	 * 
@@ -223,21 +227,21 @@ var pluginName = "ik_suggest",
 	 * @param {string} str - Search string.
 	 */
 	Plugin.prototype.getSuggestions = function (arr, str) {
-		
+
 		var r, pattern, regex, len, limit;
-		
+
 		r = [];
 		pattern = '(\\b' + str + ')';
 		regex = new RegExp(pattern, 'gi');
 		len = this.options.minLength;
 		limit = this.options.maxResults;
-			
+
 		if (str.length >= len) {
-			for (var i = 0, l = arr.length; i < l ; i++) {
-				if (r.length > limit ) {
+			for (var i = 0, l = arr.length; i < l; i++) {
+				if (r.length > limit) {
 					break;
 				}
-				if ( regex.test(arr[i]) ) {
+				if (regex.test(arr[i])) {
 					r.push(arr[i].replace(regex, '<span>$1</span>'));
 				}
 				if (r.length) {
@@ -248,21 +252,21 @@ var pluginName = "ik_suggest",
 
 		return r;
 
-		
-		
+
+
 	};
 
-	$.fn[pluginName] = function ( options ) {
-		
+	$.fn[pluginName] = function (options) {
+
 		return this.each(function () {
-			
-			if ( !$.data(this, pluginName )) {
-				$.data( this, pluginName,
-				new Plugin( this, options ));
+
+			if (!$.data(this, pluginName)) {
+				$.data(this, pluginName,
+					new Plugin(this, options));
 			}
-			
+
 		});
-		
+
 	}
- 
-})( jQuery, window, document );
+
+})(jQuery, window, document);
